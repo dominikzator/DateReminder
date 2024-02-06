dd34208c32d132206dafc9e5b1be079b92c77714
Zatii <dominikzator@gmail.com>
2024-02-06 22:33:44 +0100

    added ConfirmationWindow with 2 buttons and 2 actions(onConfirm, onDecline) in constructor

---
+++ ConfirmationWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DateReminder
+{
+    /// <summary>
+    /// Interaction logic for ConfirmationWindow.xaml
+    /// </summary>
+    public partial class ConfirmationWindow : Window
+    {
+        private Action? onConfirm, onDecline;
+
+        public ConfirmationWindow(string message, Action? onConfirm = null, Action? onDecline = null)
+        {
+            InitializeComponent();
+            MessageLabel.Content = message;
+            this.onConfirm = onConfirm;
+            this.onDecline = onDecline;
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("YesButton_Click");
+            onConfirm?.Invoke();
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("NoButton_Click");
+            onDecline?.Invoke();
+        }
+    }
+}
